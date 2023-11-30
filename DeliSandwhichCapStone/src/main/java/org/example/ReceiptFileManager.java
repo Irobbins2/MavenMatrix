6983067ae4acf9c7174764ab08408ffb98417987
Adam Juarez <adamjuarez@Adams-MacBook-Pro.local>
2023-11-30 09:25:54 -0600

    UpdatedRFM

--- DeliSandwhichCapStone/src/main/java/org/example/ReceiptFileManager.java
+++ DeliSandwhichCapStone/src/main/java/org/example/ReceiptFileManager.java
@@ -1,28 +1,22 @@
 package org.example;
 
-
 import java.io.*;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 
+public class ReceiptFileManager{
 
-
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-
-public class ReceiptFileManager {
     private String fileName;
+    private ReceiptFileManager receipt;
 
-     public ReceiptFileManager(String fileName) {
+
+
+    public ReceiptFileManager(String fileName) {
         this.fileName = fileName;
     }
-    ReceiptFileManager receipt =  new ReceiptFileManager("src/main/resources/Receipt.csv");
 
-    public void createReceipt(String[] items, double[] prices, double totalPrice) {
+    public void createReceipt(String[] items, double[] prices, double totalPrice, ArrayList<RegularToppings> regularTList, ArrayList<String> sauceList, ArrayList<Drink> drinks, ArrayList<Chips> chips) {
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(fileName, true))) {
             LocalDateTime currentDateTime = LocalDateTime.now();
             DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
@@ -32,13 +26,9 @@ public class ReceiptFileManager {
             writer.write("Item/Price\n");
 
             // Write items and prices
-
-            writer.write("Item,Price\n");
-
             for (int i = 0; i < items.length; i++) {
                 writer.write(items[i] + "," + prices[i] + "\n");
             }
-
 
             boolean hasRegularToppings = !regularTList.isEmpty();
             boolean hasSauces = !sauceList.isEmpty();
@@ -83,17 +73,12 @@ public class ReceiptFileManager {
 
             writer.write("Total Price:" + totalPrice + "\n\n");
 
-            writer.write("Total Price:," + totalPrice + "\n");
-            writer.write("\n");
-
-
             System.out.println("Receipt details added to file: " + fileName);
         } catch (IOException e) {
             System.out.println("An error occurred while writing to the receipt file.");
             e.printStackTrace();
         }
     }
-
 
     public ArrayList<String> getReceipt() {
         ArrayList<String> receipt = new ArrayList<>();
@@ -111,6 +96,3 @@ public class ReceiptFileManager {
         return receipt;
     }
 }
-
-}
-

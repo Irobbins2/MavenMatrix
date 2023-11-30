7836532123057dcf07c392014137a6aa39e2b40a
Adam Juarez <adamjuarez@Adams-MacBook-Pro.local>
2023-11-30 09:29:20 -0600

    UpdatedUI

--- DeliSandwhichCapStone/src/main/java/org/example/UserInterface.java
+++ DeliSandwhichCapStone/src/main/java/org/example/UserInterface.java
@@ -1,22 +1,14 @@
 package org.example;
 
-
 import org.example.*;
 
 import java.util.ArrayList;
 import java.util.Scanner;
 
-import java.util.InputMismatchException;
-import java.util.Scanner;
-
-import static org.example.Menu.displayWelcomeMessage;
-
 public class UserInterface {
     private static Checkout checkout = new Checkout();
-    private static Scanner scanner = new Scanner(System.in);
 
     public static void runOrderingSystem() {
-
         Scanner scanner = new Scanner(System.in);
         boolean addingItems = true;
 
@@ -92,7 +84,7 @@ public class UserInterface {
             extraCheese = "yes".equalsIgnoreCase(scanner.next());
         }
 
-        Sandwich sandwich = new Sandwich(sandwichSize);
+        Sandwich sandwich = new Sandwich(sandwichSize, breadType);
 
         if (premiumMeat) {
             PremiumToppings premiumMeatTopping = new PremiumToppings("Premium Meat", PremiumToppingType.MEAT);
@@ -143,195 +135,48 @@ public class UserInterface {
     }
 
 
-        while (true) {
-            displayWelcomeMessage();
-            displayHomeScreen();
-            int choice = getIntegerInput();
-
-            switch (choice) {
-                case 1:
-                    newOrder();
-                    break;
-                case 0:
-                    exitApplication();
-                    break;
-                default:
-                    System.out.println("Invalid choice. Please enter a valid option.");
-            }
-        }
-    }
-
-    private static void displayHomeScreen() {
-
-        System.out.println("1) New Order");
-        System.out.println("0) Exit");
-    }
-
-    private static void newOrder() {
-        while (true) {
-            displayOrderScreen();
-            int choice = getIntegerInput();
-
-            switch (choice) {
-                case 1:
-                    orderSandwich();
-                    break;
-                case 2:
-                    orderDrinks();
-                    break;
-                case 3:
-                    orderChips();
-                    break;
-                case 4:
-                    printOrderSummary();
-                    break;
-                case 0:
-                    cancelOrder();
-                    return;
-                default:
-                    System.out.println("Invalid choice. Please enter a valid option.");
-            }
-        }
-    }
-
-    private static void displayOrderScreen() {
-        System.out.println("Explore our menu and create your perfect sandwich combination.");
-        System.out.println("We use the freshest ingredients to ensure a delicious experience.");
-        System.out.println("1) Add Sandwich");
-        System.out.println("2) Add Drink");
-        System.out.println("3) Add Chips");
-        System.out.println("4) Checkout");
-        System.out.println("0) Cancel Order");
-    }
-
-    private static void orderSandwich() {
-        try {
-            System.out.println("How many sandwiches do you want?");
-            int numberOfSandwiches = getIntegerInput();
-
-            for (int sandwichNumber = 1; sandwichNumber <= numberOfSandwiches; sandwichNumber++) {
-                System.out.println("Sandwich #" + sandwichNumber);
-
-                System.out.println("Select type of bread (choose from the Menu): ");
-                Menu.displayBreadType();
-                String breadType = scanner.next();
-
-                System.out.println("Please select sandwich size (4/8/12 inches): ");
-                int sandwichSize = getIntegerInput();
-
-                System.out.println("Do you want premium meat? (yes/no): ");
-                boolean premiumMeat = getYesNoInput();
-
-                System.out.println("Do you want premium cheese? (yes/no): ");
-                boolean premiumCheese = getYesNoInput();
-
-                Sandwich sandwich = new Sandwich(sandwichSize, breadType);
-                checkout.addSandwich(sandwich);
-
-                addRegularToppings(sandwich);
-
-                if (premiumMeat) {
-                    addPremiumToppings(sandwich, PremiumToppingType.MEAT);
-                }
-
-                if (premiumCheese) {
-                    addPremiumToppings(sandwich, PremiumToppingType.CHEESE);
-                }
-            }
-        } catch (Exception e) {
-            System.out.println("Error: " + e.getMessage());
-        }
-    }
-
-    private static void addPremiumToppings(Sandwich sandwich, PremiumToppingType toppingType) {
-        try {
-            System.out.println("Adding Premium " + toppingType.toString().toLowerCase() + ":");
-            for (String premiumTopping : Menu.getPremiumToppings(toppingType.toString().toLowerCase())) {
-                System.out.println("Do you want " + premiumTopping + "? (yes/no): ");
-                boolean addTopping = getYesNoInput();
-
-                if (addTopping) {
-                    sandwich.addTopping(new PremiumToppings(premiumTopping, toppingType));
-                }
-            }
-        } catch (Exception e) {
-            System.out.println("Error: " + e.getMessage());
-        }
-    }
-
-    private static void addRegularToppings(Sandwich sandwich) {
-        try {
-            Menu.displayRegularToppings();
-
-            for (String regularTopping : Menu.getRegularToppings()) {
-                System.out.println("Add " + regularTopping + "? (yes/no): ");
-                boolean addTopping = getYesNoInput();
-
-                if (addTopping) {
-                    sandwich.addTopping(new RegularToppings(regularTopping));
-                }
-            }
-        } catch (Exception e) {
-            System.out.println("Error: " + e.getMessage());
-        }
-    }
-
-
     private static void orderDrinks() {
-        try {
-            System.out.println("Do you want to add drinks? (yes/no): ");
-            boolean addDrinks = getYesNoInput();
+        Scanner scanner = new Scanner(System.in);
 
-            if (addDrinks) {
-                System.out.println("Please specify the number of drinks: ");
-                int numberOfDrinks = getIntegerInput();
+        System.out.println("Do you want to add drinks? (yes/no): ");
+        boolean addDrinks = "yes".equalsIgnoreCase(scanner.next());
 
-                for (int i = 0; i < numberOfDrinks; i++) {
-                    System.out.println("Drink #" + (i + 1));
+        if (addDrinks) {
+            System.out.println("Please specify the number of drinks: ");
+            int numberOfDrinks = scanner.nextInt();
 
-                    Menu.displayDrinkBrands();
+            for (int i = 0; i < numberOfDrinks; i++) {
+                System.out.println("Enter drink name for drink #" + (i + 1) + ": ");
+                String drinkBrand = scanner.next();
 
-                    System.out.println("Enter drink brand for drink #" + (i + 1) + ": ");
-                    String drinkBrand = scanner.next();
+                System.out.println("Enter drink size for drink #" + (i + 1) + " (small/medium/large): ");
+                String drinkSize = scanner.next();
 
-                    System.out.println("Enter drink size for drink #" + (i + 1) + " (small/medium/large): ");
-                    String drinkSize = scanner.next();
-
-                    Drink drink = new Drink(drinkSize, drinkBrand);
-                    checkout.addDrink(drink);
-                }
+                Drink drink = new Drink(drinkSize, drinkBrand);
+                checkout.addDrink(drink);
             }
-        } catch (Exception e) {
-            System.out.println("Error: " + e.getMessage());
         }
     }
 
     private static void orderChips() {
-        try {
-            System.out.println("Do you want to add chips? (yes/no): ");
-            boolean addChips = getYesNoInput();
+        Scanner scanner = new Scanner(System.in);
 
-            if (addChips) {
-                System.out.println("Please specify the number of chips: ");
-                int numberOfChips = getIntegerInput();
+        System.out.println("Do you want to add chips? (yes/no): ");
+        boolean addChips = "yes".equalsIgnoreCase(scanner.next());
 
-                for (int i = 0; i < numberOfChips; i++) {
-                    System.out.println("Chips #" + (i + 1));
+        if (addChips) {
+            System.out.println("Please specify the number of chips: ");
+            int numberOfChips = scanner.nextInt();
 
-                    Menu.displayChipsBrands();
+            for (int i = 0; i < numberOfChips; i++) {
+                System.out.println("Enter chips type for chips #" + (i + 1) + ": ");
+                String chipsBrand = scanner.next();
 
-                    System.out.println("Enter chips brand for chips #" + (i + 1) + ": ");
-                    String chipsBrand = scanner.next();
-
-                    Chips chips = new Chips(chipsBrand);
-                    checkout.addChips(chips);
-                }
+                Chips chips = new Chips(chipsBrand);
+                checkout.addChips(chips);
             }
-        } catch (Exception e) {
-            System.out.println("Error: " + e.getMessage());
         }
     }
-
 
     private static void generateReceipt() {
         ArrayList<Sandwich> sandwiches = checkout.getSandwiches();
@@ -373,48 +218,5 @@ public class UserInterface {
         for (String line : receiptDetails) {
             System.out.println(line);
         }
-
-    private static void printOrderSummary() {
-        try {
-            checkout.listAmountOfItems();
-            System.out.println("Total Price: $" + checkout.totalPrice());
-        } catch (Exception e) {
-            System.out.println("Error: " + e.getMessage());
-        }
-    }
-
-    private static int getIntegerInput() {
-        while (true) {
-            try {
-                return scanner.nextInt();
-            } catch (InputMismatchException e) {
-                System.out.println("Invalid input. Please enter a valid integer.");
-                scanner.next();
-            }
-        }
-    }
-
-    private static boolean getYesNoInput() {
-        while (true) {
-            String input = scanner.next().toLowerCase();
-            if (input.equals("yes")) {
-                return true;
-            } else if (input.equals("no")) {
-                return false;
-            } else {
-                System.out.println("Invalid input. Please enter 'yes' or 'no'.");
-            }
-        }
-    }
-
-    private static void cancelOrder() {
-        checkout = new Checkout();
-        System.out.println("Order canceled. Returning to the home screen.");
-    }
-
-    private static void exitApplication() {
-        System.out.println("Exiting the application. Goodbye!");
-        System.exit(0);
-
     }
 }
